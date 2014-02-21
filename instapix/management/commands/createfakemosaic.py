7434287b224366c636c86eb1896236409a36c910
Antoine Lenoir <contact@antoine-lenoir.com>
2014-02-21 14:58:55 +0100

    assos fix

--- instapix/management/commands/createfakemosaic.py
+++ instapix/management/commands/createfakemosaic.py
@@ -11,11 +11,12 @@ class Command(BaseCommand):
         for mosaic in Mosaic.objects.all():
             img = Image.new('RGB',(620,620), "white")
             for pixel in mosaic.pixels.filter(pic__isnull=False):
-                filepath = settings.MEDIA_ROOT + '/pics/%s_%s.jpg' % (mosaic.pixel_size,pixel.pic.id)
-                
                 print '-------------'
                 print pixel.pic.id
                 print mosaic.pixel_size
+                filepath = settings.MEDIA_ROOT + '/pics/%s_%s.jpg' % (mosaic.pixel_size,pixel.pic.id)
+                
+                
                 print pixel.x
                 print pixel.y
                 print '-------------'
